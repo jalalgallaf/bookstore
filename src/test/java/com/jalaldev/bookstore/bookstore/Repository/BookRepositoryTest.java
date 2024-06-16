e335546f9268ca284e5e9c6cb763f7de0b465837
sgallaf <jalal.gallaf@gmail.com>
2024-06-16 17:16:08 +0300

    Adding DTOs

--- src/test/java/com/jalaldev/bookstore/bookstore/Repository/BookRepositoryTest.java
+++ src/test/java/com/jalaldev/bookstore/bookstore/Repository/BookRepositoryTest.java
@@ -1,37 +1,6 @@
 package com.jalaldev.bookstore.bookstore.Repository;
 
-import com.jalaldev.bookstore.bookstore.model.Book;
-import com.jalaldev.bookstore.bookstore.repositroy.BookRepository;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validator;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import java.util.List;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.util.AssertionErrors.assertFalse;
-
-
 public class BookRepositoryTest {
-
-    @Test
-    void test2(){
-    }
-
-
-
-
-    @Test
-    void test(){
-    }
-
 
 
 }
